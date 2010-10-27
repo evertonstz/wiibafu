1c6492c94c7061b1d06a7d5d54bb38ffa5a54608
dynup <none@none>
2010-10-28 00:06:12 +0200

    Switch version number to 0.3

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationDomain("wiibafu.codeplex.com");
     QCoreApplication::setOrganizationName("Kai Heitkamp");
     QCoreApplication::setApplicationName("Wii Backup Fusion");
-    QCoreApplication::setApplicationVersion("0.1");
+    QCoreApplication::setApplicationVersion("0.3");
 
     WiiBaFu wiibafu;
     wiibafu.show();
