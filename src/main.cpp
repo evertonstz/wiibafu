852f373bdf225a38b0d9fd832365312aa2ed9369
dynup <none@none>
2010-11-25 22:29:51 +0100

    Version to 0.8.4

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationDomain("wiibafu.codeplex.com");
     QCoreApplication::setOrganizationName("Kai Heitkamp");
     QCoreApplication::setApplicationName("Wii Backup Fusion");
-    QCoreApplication::setApplicationVersion("0.8.3");
+    QCoreApplication::setApplicationVersion("0.8.4");
 
     WiiBaFu wiibafu;
     wiibafu.show();
