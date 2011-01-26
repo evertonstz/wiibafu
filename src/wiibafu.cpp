559b9e561d3f87a42e8a777cd132ca714342ee9e
dynup <none@none>
2011-01-26 19:30:11 +0100

    About dialog text adapted

--- src/wiibafu.cpp
+++ src/wiibafu.cpp
@@ -1865,7 +1865,7 @@ void WiiBaFu::on_menuHelp_About_triggered() {
         "<p><font color='red'>I don't support piracy! If you copy games with this software,"
         "<br>you must have the original and it's for your private use only!</font color>"
         "<p>Big thanks to the trolls at Trolltech Norway for his excellent Qt toolkit and the guys at Nokia for the continuation, thanks to Dirk Clemens (Wiimm) for his great ISO tools!"
-        " Thanks to Patrick for the french translation, Antonio Lopez for the spanish translation and Michael Krause for the norwegian translation!"
+        " Thanks to Patrick for the french translation, Michel Schaart for the dutch translation, Antonio Lopez for the spanish translation and Michael Krause for the norwegian translation!"
         "<p><i>Dedicated in memory of my father G&uuml;nter Heitkamp (28.07.1935 - 06.10.2009)</i>"));
 }
 
